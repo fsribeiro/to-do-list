7b694c5a2626fbaa6199c4b6e41b2f79e190a07d
Fernando <fernando.ribeiro@ramper.com.br>
2022-01-14 14:04:28 -0300

    add module user

--- modules/app/src/app.module.ts
+++ modules/app/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGO_API_URI, {
       connectionName: process.env.MONGO_API_DOCKER_SERVICE,
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
